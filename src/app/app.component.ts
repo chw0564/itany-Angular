f98e7b37e1b303ca2035a6b13fa9e0ad396090e1
zbwroom@163.com <zbwroom@163.com>
2017-11-22 14:28:34 +0800

    day01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 
+// 一个组件包含 .ts  .html .css
+//     三个文件一般采用相同的名称
 @Component({
   // 自定义 AngularMl 标签
   selector: 'app-root',
+  // 该组件所对应的模板页
   templateUrl: './app.component.html',
+  //  该组件所对应的 局部样式表
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'itany';
 }
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,27 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+// 自定 TS 文件的导入
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// 判断项目已何种方式进行 启动
 if (environment.production) {
   enableProdMode();
 }
 
-// 完成  程序的引导模块加载 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.log(err));
+
+// 返回一个引导组件
+let pbd = platformBrowserDynamic();
+// 完成 程序的引导模块加载 
+pbd.bootstrapModule(AppModule)
+   .catch((error)=>{
+      console.log(error);
+   });
+
+// (error)=>{console.log(error);}
+//  error => console.log(error)
+
+// if(true) console.log(1)
